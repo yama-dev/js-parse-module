645ac82ed26d6b7d45fe875ce944596b0c4c206e
yama-dev <tatsuya.yamamoto69@gmail.com>
2019-07-20 16:16:55 +0900

    adjsut scirpt.

--- src/js-parse-module.js
+++ src/js-parse-module.js
@@ -1,77 +1,31 @@
-/*!
- * JS PARSE_MODULE (JavaScript Library)
- *   js-parse-module.js
- * Version 0.0.4
- * Repository https://github.com/yama-dev/js-parse-module
- * Author yama-dev
- * Licensed under the MIT license.
- */
+import {
+  Str2AddLink,
+  Str2AddLinkHashtag,
+  Str2DateFormat,
+  Str2Mustache,
+  Str2zeroPadding
+} from './core/';
 
 export class PARSE_MODULE {
 
-  static Str2AutoLink(str, target='_blank') {
-
-    if(!str) return false
-
-    const regexp_url = /((h?)(ttps?:\/\/[a-zA-Z0-9.\-_@:/~?%&;=+#',()*!]+))/g;
-    const regexp_makeLink = function(all, url, h, href) {
-      return `<a href="${h + href}" target="${target}">${url}</a>`;
-    }
-    return str.replace(regexp_url, regexp_makeLink);
+  static Str2AddLink(str, target='_blank') {
+    return Str2AddLink(str, target);
   }
 
-  static Str2AutoLinkHashtag(str, target='_blank') {
-
-    if(!str) return false
-
-    return str.replace(/\#(\S*)\s?/g,'<a href="https://twitter.com/search?q=%23$1" target="'+target+'">#$1</a>');
+  static Str2AddLinkHashtag(str, target='_blank') {
+    return Str2AddLinkHashtag(str, target);
   }
 
   static Str2DateFormat(str){
-
-    if(!str) return false
-
-    let _data = str.split(/\D/);
-
-    if(_data.length >= 4){
-      let _data_copy = [];
-      _data.map((val, index)=>{
-        if(val == '') return false
-        _data_copy.push(val);
-      });
-      _data = _data_copy;
-    }
-
-    _data = _data.map((val,i)=>{
-      let _return = val;
-      if(Number(val) <= 9) _return = '0' + Number(val);
-      return _return
-    });
-
-    let _format = '';
-    _format = _data.join('-');
-
-    return _format
+    return Str2DateFormat(str);
   }
 
-  static Str2Mustache(template, obj){
+  static Str2Mustache(template, obj, removetag = true){
+    return Str2Mustache(template, obj, removetag);
+  }
 
-    if(!template) return false
-
-    for (let _obj in obj) {
-      let _reg   = new RegExp('({{.?' + _obj + '.?}})','g');
-      let _regIn = new RegExp('{{.?(' + _obj + ').?}}','g');
-
-      template.match(_regIn);
-      let _regInStr = RegExp.$1;
-
-      template = template.replace(_regIn, obj[_regInStr]);
-    }
-
-    let _format = '';
-    _format = template;
-
-    return _format
+  static Str2zeroPadding(str, length, padstr = '0'){
+    return Str2zeroPadding(str, length, padstr);
   }
 
 }
